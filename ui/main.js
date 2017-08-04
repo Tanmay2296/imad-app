5fdb89c6372e1400c9b43856ab700eb17bb9c775
Tanmay N T <tanmayrox1996@gmail.com>
2017-08-04 23:39:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,10 +13,26 @@ button.onclick = function() {
         if(request.readyState === XMLHttpRequest.DONE) {
         //Take some action
     if(request.status === 200) {    
+      var request = new XMLHttpRequest();
+    
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE) {
+        //Take some action
+    if(request.status === 200) {    
     var counter = request.responseText;        
     var span = document.getElementById("count");
     span.innerHTML = counter.toString();
         }
+        }        
+        }
+        
+    };
+    //request
+    request.open("GET","http://tanmayrox1996.imad.hasura-app.io/counter",true);
+    request.send(null);
+    
         
         }
         
