cdcded383fd2f805dc8303785001558212cdd688
Tanmay N T <tanmayrox1996@gmail.com>
2017-08-04 23:25:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/submit-name/:name', function(req, res) {
+   var name = req.params.name;
+   
+   name.push(name);
+   
+   res.send(JSON.stringify(names));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
