6074595d4d00986d9589a0c81b5106dfee07de42
Tanmay N T <tanmayrox1996@gmail.com>
2017-08-05 00:15:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,10 @@ console.log('Loaded!');
 
 var button = document.getElementById("counter");
 
-button.onclick = function() {
-    
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit");
+submit.onclick = function() {
     //Make a request to the counter endpoint
     var request = new XMLHttpRequest();
     
@@ -13,10 +15,7 @@ button.onclick = function() {
         if(request.readyState === XMLHttpRequest.DONE) {
         //Take some action
     if(request.status === 200) {    
-    var nameInput = document.getElementById("name");
-var name = nameInput.value;
-var submit = document.getElementById("submit");
-submit.onclick = function() {
+  
     //make a request to server
     // display
     var names = request.responseText;
