d0ef3b63920c9c04743cab8e2b3bcaa57f45b8df
Tanmay N T <tanmayrox1996@gmail.com>
2017-08-12 19:36:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,6 +129,7 @@ app.get('/articles/:articleName', function (req, res) {
     // article name = article - one
     // articles[articleName] == {} content object for article one
     //Select * FROM article where title = '';DELETE WHERE a = 'asdf'
+    //the code below protects our database from hacking and deleting our table
     pool.query("SELECT * FROM article WHERE title = $1" , [req.params.articleName] , function (err, result) {
         
          if(err){
