b6b67bf1bd2ac2c1a34ee074f06459796a8d70a2
Tanmay N T <tanmayrox1996@gmail.com>
2017-08-19 17:15:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var hashed = require('crypto');
+
 var config = {
     user:'tanmayrox1996',
     database:'tanmayrox1996',
@@ -67,6 +69,18 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input, salt) {
+     var hashed = crypto.pbkdf25Sync(input, salt, 10000, 512,'sha512');
+     return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input', function (req, res){
+   var hashedString = hash(req.params.input, 'this is some random string');
+   res.send(hashedString);
+    
 });
 
 var pool = new Pool(config);
