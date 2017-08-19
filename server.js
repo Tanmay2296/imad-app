667436946415b46d9c0c0d3db9a0859c49056333
Tanmay N T <tanmayrox1996@gmail.com>
2017-08-19 17:18:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var hashed = require('crypto');
+var crypto = require('crypto');
 
 var config = {
     user:'tanmayrox1996',
